780544c72d958c5db4df234d8bfe763e5ed22885
nydia23 <148511314+nydia23@users.noreply.github.com>
2023-11-20 23:13:41 -0600

    Add files via upload

---
+++ Chat/dbConfig.js
@@ -0,0 +1,14 @@
+// dbConfig.js
+const config = {
+    user: 'tu_usuario',
+    password: 'tu_contraseña',
+    server: 'tu_servidor',
+    database: 'tu_base_de_datos',
+    options: {
+      encrypt: true,
+      trustServerCertificate: true,
+    },
+  };
+  
+  module.exports = config;
+  
